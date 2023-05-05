e24e0d32e6889d7da9252cc82e34f657d967b96b
CristinaDemontis <s281766@studenti.polito.it>
2023-05-05 11:29:31 +0200

    Lab 07 ricorsione blackout

--- src/main/java/it/polito/tdp/poweroutages/DAO/PowerOutageDAO.java
+++ src/main/java/it/polito/tdp/poweroutages/DAO/PowerOutageDAO.java
@@ -35,5 +35,7 @@ public class PowerOutageDAO {
 		return nercList;
 	}
 	
+	
+	
 
 }
--- src/main/java/it/polito/tdp/poweroutages/model/Model.java
+++ src/main/java/it/polito/tdp/poweroutages/model/Model.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.poweroutages.model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import it.polito.tdp.poweroutages.DAO.PowerOutageDAO;
@@ -15,5 +16,21 @@ public class Model {
 	public List<Nerc> getNercList() {
 		return podao.getNercList();
 	}
-
+	
+	public List<PowerOutageDAO> pre_ricorsione(Nerc azienda, int anni, int ore){
+		int best_cnt_persone = 0; 
+		
+		List<PowerOutageDAO> parziale = new LinkedList<>(); 
+		List<PowerOutageDAO> soluzione_migliore = ricorsione(parziale, anni, ore ); 
+		
+		return soluzione_migliore; 
+	}
+	
+	
+	public List<PowerOutageDAO> ricorsione(List<PowerOutageDAO> parziale_migliore, int anni, int ore ){ // rivedere i paramentri 
+		
+	
+		
+		return null; 
+	}
 }
